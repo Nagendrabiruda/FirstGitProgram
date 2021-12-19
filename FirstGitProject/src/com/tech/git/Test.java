d3f8349b2e24245dfd8703810bc45c2457bd883e
Nagendra <Nagendra@DESKTOP-4TA60V2>
2021-12-19 18:46:14 +0530

    First commit

---
+++ FirstGitProject/src/com/tech/git/Test.java
@@ -0,0 +1,13 @@
+package com.tech.git;
+
+public class Test {
+
+	public static void main(String args[]) {
+		String hungry = null;
+		hungry = "Yes";
+		if (hungry.equalsIgnoreCase("Yes"))
+			System.out.println("Eating Apple");
+		else
+			System.out.println("Sitting Idle");
+	}
+}
